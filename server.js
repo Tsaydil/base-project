4159b7c84e7d0b26f7b2123e5a992861f4677a09
Tsaydil <tsaydil@gmail.com>
2021-01-06 13:33:35 +0300

    Changed server.listen to dynamic port.

--- server.js
+++ server.js
@@ -92,10 +92,15 @@ app.prepare()
             return handle(req, res);
         })
 
-        server.listen(3000, (err) => {
+        server.listen(process.env.PORT || 5000, (err) => {
             if (err) throw err
-            console.log('> Ready on http://localhost:3000')
+            console.log('> Ready on http://localhost:5000')
         })
+
+        /*server.listen(3000, (err) => {
+            if (err) throw err
+            console.log('> Ready on http://localhost:5000')
+        })*/
     })
     .catch((ex) => {
         console.error(ex.stack)
